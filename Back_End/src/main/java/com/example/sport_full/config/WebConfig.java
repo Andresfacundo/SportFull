0059a7da7c0bf40efc8b7bcaf0e3228f4c0e4e0a
SateHs <santiagoag365@gmail.com>
2024-09-18 09:40:16 -0500

    Update maps to Company and not Fields

--- Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
+++ Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
@@ -10,9 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
 
     }
 }
+
